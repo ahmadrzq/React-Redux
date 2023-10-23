baba1eeb351c44fc6a5d4ce3e3fe46c72ed2c45e
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-10-23 10:48:03 +0700

    import testreducercopy

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import TestReducer from "./Reducers/TestReducer";
+import TestReducerCopy from "./Reducers/TestReducerCopy";
 
 export const store = configureStore({
   reducer: {
-    TestReducer
+    TestReducer,
+    TestReducerCopy,
   },
 });
